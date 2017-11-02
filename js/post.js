4a71ee8bdff732304fd07abb4c847233787da76c
Leandro Pereira da Cruz <leandropereiradacruz@gmail.com>
2017-11-02 15:15:54 -0200

    testing post

--- js/post.js
+++ js/post.js
@@ -1,3 +1,23 @@
 $(function(){
-    $('#header').load('./page/header.html');
+    $.getJSON("data/posts.json", function(data) {
+        var val = data[1];
+        $("<div class=\"row justify-content-center\">"+
+            "<div class=\"col\">"+
+                "<a href=\"post/" + val.id + ".html\">" +
+                    "<h2>" + val.title + "</h2>" +
+                "</a>" +
+                "<h5>" +
+                    "<a class=\"btn btn-primary btn-facebook btn-sm\">"+
+                        "<i class=\"fa fa-facebook\"></i> Compartilhar"+
+                    "</a> "+
+                    val.created + " " + val.category +
+                "</h5>"+
+                "<div>" + val.body + "</div>"+
+                "<h5><Deixe seu comentário abaixo:/h5>"+
+                "<br/>"+
+                "<hr/>"+
+            "<div/>"+
+           "</div>"           
+        ).appendTo("#posts");
+    });
 })
